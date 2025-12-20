c99af823ee5e782d032932ce5cdf2e0b7f79beb8
umoba <umi.oba28@gmail.com>
2025-12-20 12:09:52 +0900

    updated for building purposes

--- src/sub/tongue.cpp
+++ src/sub/tongue.cpp
@@ -1,6 +1,7 @@
 #include "main.h"// IWYU pragma: keep
 #include "globals.h"// IWYU pragma: keep
 #include "subs_headers/tongue.h"
+#include "subs_headers/intakes.h"
 #include "pros/misc.h"
 using namespace Robot;
 using namespace Robot::Globals;
@@ -11,7 +12,7 @@ void Tongue::run() {
   if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
     tonguemech.toggle();
     if (tonguemech.is_extended() && sprocketIntake.get_target_velocity() != 0) {
-      sprocketIntake.move_velocity(MAX_55_VELOCITY);
+      sprocketIntake.move_velocity(subsystem.intake.MAX_55_VELOCITY);
     }
   }
 }
